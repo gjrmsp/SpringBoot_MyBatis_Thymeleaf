319c00016d76c029ffc4eb25185a0f3858dcb97c
gjrmsp <gjrmsp@naver.com>
2021-05-10 17:16:36 +0900

    Initial commit

---
+++ src/main/java/com/sana/s1/SpringBootMyBatisThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.sana.s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyBatisThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyBatisThymeleafApplication.class, args);
+	}
+
+}
