672a61d7afb01766032618183420bfcd741dbcb9
gjrmsp <gjrmsp@naver.com>
2021-05-11 12:42:14 +0900

    update

--- src/main/java/com/sana/s1/home/HomeController.java
+++ src/main/java/com/sana/s1/home/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.sana.s1.board.BoardVO;
+
 /**
 ===========================
 	Thymeleaf Project
@@ -15,8 +17,13 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String home(Model model) {
-		model.addAttribute("message", "Thymeleaf Project");
+		//model.addAttribute("message", "Thymeleaf Project");
+		BoardVO boardVO = new BoardVO();
+		boardVO.setNum(1L);
+		boardVO.setTitle("title");
+		boardVO.setWriter("writer");
+		
+		System.out.println(boardVO.toString());
 		return "index";
 	}
-
 }
