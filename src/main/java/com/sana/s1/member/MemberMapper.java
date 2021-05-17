8dcbf43a1146e203264efc277348572a2506c2f5
gjrmsp <gjrmsp@naver.com>
2021-05-17 16:43:43 +0900

    update

---
+++ src/main/java/com/sana/s1/member/MemberMapper.java
@@ -0,0 +1,14 @@
+package com.sana.s1.member;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberMapper {
+
+	public int setJoin(MemberVO memberVO) throws Exception;
+
+	public int setJoinFile(MemberFileVO memberFileVO) throws Exception;
+
+	public MemberVO getLogin(MemberVO memberVO) throws Exception;
+	
+}
