5e1db7bb66717da28e341435067cfe64a8b23a48
gjrmsp <gjrmsp@naver.com>
2021-05-21 16:50:52 +0900

    update

--- src/main/java/com/sana/s1/member/MemberService.java
+++ src/main/java/com/sana/s1/member/MemberService.java
@@ -2,6 +2,7 @@ package com.sana.s1.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.sana.s1.util.FileManager;
@@ -11,9 +12,41 @@ public class MemberService {
 
 	@Autowired
 	private MemberMapper memberMapper;
-	
+
 	@Autowired
 	private FileManager fileManager;
+
+	//검증 메서드
+	public boolean memberError(MemberVO memberVO, Errors errors) throws Exception {
+		boolean result = false;
+
+		//기본 제공 검증  결과
+		//		if(errors.hasErrors()) {
+		//			result = true;
+		//		}
+		result = errors.hasErrors();
+
+		//password가 일치 여부 검증
+		if(!memberVO.getPassword().equals(memberVO.getPassword1())) {
+			errors.rejectValue("password1", "memberVO.password.notEqual");
+			//(form path, field 명, properties의 code(key));
+			result=true;
+		}
+
+		//UserName 중복 여부
+		MemberVO checkMember = memberMapper.getUsername(memberVO);
+
+		//checkMember 가 null이면 중복 X
+		//checkMember 가 null이 아니면 중복
+		if(checkMember != null) {
+			errors.rejectValue("username", "member.id.equal");
+			result = true;
+		}
+
+		//admin, adminstrator, root
+
+		return result;
+	}
 
 	public int setJoin(MemberVO memberVO, MultipartFile multipartFile) throws Exception {
 
@@ -22,7 +55,7 @@ public class MemberService {
 
 		//2. HDD에 저장
 		String filePath = "upload/member/";
-		
+
 		if(multipartFile.getSize() != 0) {
 			String fileName = fileManager.save(multipartFile, filePath);
 			System.out.println(fileName);
@@ -31,7 +64,7 @@ public class MemberService {
 			memberFileVO.setOriName(multipartFile.getOriginalFilename());
 			memberFileVO.setUsername(memberVO.getUsername());
 
-		//3. MemberFiles table 저장
+			//3. MemberFiles table 저장
 			result = memberMapper.setJoinFile(memberFileVO);
 		}
 		return result;
--- src/main/java/com/sana/s1/member/MemberVO.java
+++ src/main/java/com/sana/s1/member/MemberVO.java
@@ -1,14 +1,23 @@
 package com.sana.s1.member;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import lombok.Data;
 
 @Data
 public class MemberVO {
 
 	private String username;
+	private String password1;
+	@Length(max = 10, min = 2)
 	private String password;
+	@NotEmpty
 	private String name;
+	@NotEmpty
 	private String email;
+	@NotEmpty
 	private String phone;
 
 }
