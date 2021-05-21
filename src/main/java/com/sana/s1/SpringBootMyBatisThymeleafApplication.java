b29a56db113f1d4f1175d196a2e79ec7f13ed493
gjrmsp <gjrmsp@naver.com>
2021-05-21 16:59:15 +0900

    update

--- src/main/java/com/sana/s1/SpringBootMyBatisThymeleafApplication.java
+++ src/main/java/com/sana/s1/SpringBootMyBatisThymeleafApplication.java
@@ -2,7 +2,9 @@ package com.sana.s1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SpringBootMyBatisThymeleafApplication {
 
